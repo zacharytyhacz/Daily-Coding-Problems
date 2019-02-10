55c54d6695de8319a21743eae158e627d8ffa32f
zachary tyhacz <zactyh@gmail.com>
2019-02-10 04:14:59 -0500

    removed unneeded function

--- day1.py
+++ day1.py
@@ -1,13 +1,11 @@
 #This problem was recently asked by Google.
 #Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 #For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-def main():
-	numbers = [10,15,3,7,11,6,16,1]
-	k = 17;
+numbers = [10,15,3,7,11,6,16,1]
+k = 17;
 
-	for i in numbers:
-		for j in numbers:
-			if i + j == k:
-				print(str(j) + " + " + str(i) + " == " + str(k))
+for i in numbers:
+	for j in numbers:
+		if i + j == k:
+			print(str(j) + " + " + str(i) + " == " + str(k))
 
-main()	
